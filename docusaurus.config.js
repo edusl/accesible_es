1265621192098287db84bfbf3d1ee4c8654ffca0
Eduardo Sainz <esainz@paradigmadigital.com>
2020-08-27 12:13:51 +0200

    Correcciones

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -45,17 +45,9 @@ module.exports = {
           title: 'Community',
           items: [
             {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
-            },
+              label: 'Próximamente',
+              href: '#',
+            }
           ],
         },
         {
